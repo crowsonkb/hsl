35b16a1bbafaad6ebc015e086b45d1d3df31af44
Katherine Crowson <crowsonkb@gmail.com>
2018-05-11 16:45:00 -0700

    Parser cleanup

--- hsl/parse_colors.py
+++ hsl/parse_colors.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 import pyparsing as pp
 from pyparsing import pyparsing_common as ppc
 
@@ -10,31 +12,31 @@ class CSSColor:
         self.value = [a, b, c, d]
 
     def as_hsl(self):
-        if self.space in ('hsl', 'hsla'):
+        if self.space == 'hsl':
             h, s, l, _ = self.value
-        if self.space in ('rgb', 'rgba'):
+        if self.space == 'rgb':
             h, s, l = rgb_to_hsl(self.value[:3])
         return f'hsl({h * 360:.1f}, {s * 100:.1f}%, {l * 100:.1f}%)'
 
     def as_hsla(self):
-        if self.space in ('hsl', 'hsla'):
+        if self.space == 'hsl':
             h, s, l, _ = self.value
-        if self.space in ('rgb', 'rgba'):
+        if self.space == 'rgb':
             h, s, l = rgb_to_hsl(self.value[:3])
         a = self.value[3]
         return f'hsla({h * 360:.1f}, {s * 100:.1f}%, {l * 100:.1f}%, {a * 100:.1f}%)'
 
     def as_rgb(self):
-        if self.space in ('rgb', 'rgba'):
+        if self.space == 'rgb':
             r, g, b, _ = self.value
         if self.space == 'hsl':
             r, g, b = hsl_to_rgb(self.value[:3])
         return f'rgb({r * 100:.1f}%, {g * 100:.1f}%, {b * 100:.1f}%)'
 
     def as_rgba(self):
-        if self.space in ('rgb', 'rgba'):
+        if self.space == 'rgb':
             r, g, b, _ = self.value
-        if self.space in ('hsl', 'hsla'):
+        if self.space == 'hsl':
             r, g, b = hsl_to_rgb(self.value[:3])
         a = self.value[3]
         return f'rgba({r * 100:.1f}%, {g * 100:.1f}%, {b * 100:.1f}%, {a * 100:.1f}%)'
@@ -46,36 +48,46 @@ class CSSColorParser:
 
     @staticmethod
     def _build_parser():
+        def parse_float_or_percent(t):
+            v = float(t[0])
+            if t.percent:
+                return v / 100
+            return v
+
         float_or_percent = (ppc.fnumber + pp.Literal('%')('percent')) | ppc.fnumber
-        float_or_percent.addParseAction(lambda t: float(t[0]) / 100 if t.percent else float(t[0]))
+        float_or_percent.addParseAction(parse_float_or_percent)
+
+        def parse_list(space, n, t):
+            if len(t) != n:
+                raise pp.ParseFatalException('Invalid number of values in list.')
+            if space == 'hsl':
+                t[0] /= 360
+            return space + t
 
         hsl_color = pp.Suppress('hsl(') + pp.delimitedList(float_or_percent) + pp.Suppress(')')
-        hsl_color.addParseAction(lambda t: ('hsl', t[0] / 360, t[1], t[2]))
+        hsl_color.addParseAction(partial(parse_list, 'hsl', 3))
         hsla_color = pp.Suppress('hsla(') + pp.delimitedList(float_or_percent) + pp.Suppress(')')
-        hsla_color.addParseAction(lambda t: ('hsla', t[0] / 360, t[1], t[2], t[3]))
+        hsla_color.addParseAction(partial(parse_list, 'hsl', 4))
 
         rgb_color = pp.Suppress('rgb(') + pp.delimitedList(float_or_percent) + pp.Suppress(')')
-        rgb_color.addParseAction(lambda t: ('rgb', t[0], t[1], t[2]))
+        rgb_color.addParseAction(partial(parse_list, 'rgb', 3))
         rgba_color = pp.Suppress('rgba(') + pp.delimitedList(float_or_percent) + pp.Suppress(')')
-        rgba_color.addParseAction(lambda t: ('rgba', t[0], t[1], t[2], t[3]))
+        rgba_color.addParseAction(partial(parse_list, 'rgb', 4))
 
         def parse_hex_color(t):
             s = t[0]
             if len(s) not in (3, 4, 6, 8):
                 return pp.ParseFatalException('Invalid number of hex characters.')
-            space = 'rgb'
-            if len(s) in (4, 8):
-                space = 'rgba'
             if len(s) == 3 or len(s) == 4:
-                return (space,) + tuple(int(ch+ch, 16) / 255 for ch in t[0])
+                return ('rgb',) + tuple(int(ch+ch, 16) / 255 for ch in t[0])
             if len(s) == 6 or len(s) == 8:
                 r = int(s[0:2], 16) / 255
                 g = int(s[2:4], 16) / 255
                 b = int(s[4:6], 16) / 255
             if len(s) == 8:
                 a = int(s[6:8], 16) / 255
-                return space, r, g, b, a
-            return space, r, g, b
+                return 'rgb', r, g, b, a
+            return 'rgb', r, g, b
 
         hex_color = pp.Suppress('#') + pp.Word(pp.hexnums)
         hex_color.addParseAction(parse_hex_color)
