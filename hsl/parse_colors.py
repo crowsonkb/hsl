f583d7c35dd05f94d4b3dae48faa3bbb0c0cbd22
Katherine Crowson <crowsonkb@gmail.com>
2018-05-11 17:03:26 -0700

    Parser cleanup

--- hsl/parse_colors.py
+++ hsl/parse_colors.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 import pyparsing as pp
 from pyparsing import pyparsing_common as ppc
 
@@ -55,32 +57,30 @@ class CSSColorParser:
         float_or_percent = (ppc.fnumber + pp.Literal('%')('percent')) | ppc.fnumber
         float_or_percent.addParseAction(parse_float_or_percent)
 
-        def list_parser(space, n):
-            def parse(toks):
-                if len(toks) != n:
-                    raise pp.ParseFatalException('Invalid number of values in list.')
-                if space == 'hsl':
-                    toks[0] /= 360
-                return (space,) + tuple(toks)
-            return parse
+        def parse_list(space, n, t):
+            if len(t) != n:
+                raise pp.ParseFatalException('Invalid number of values in list.')
+            if space == 'hsl':
+                t[0] /= 360
+            return (space,) + tuple(t)
 
         hsl_color = pp.Suppress('hsl(') + pp.delimitedList(float_or_percent) + pp.Suppress(')')
-        hsl_color.addParseAction(list_parser('hsl', 3))
+        hsl_color.addParseAction(partial(parse_list, 'hsl', 3))
         hsla_color = pp.Suppress('hsla(') + pp.delimitedList(float_or_percent) + pp.Suppress(')')
-        hsla_color.addParseAction(list_parser('hsl', 4))
+        hsla_color.addParseAction(partial(parse_list, 'hsl', 4))
 
         rgb_color = pp.Suppress('rgb(') + pp.delimitedList(float_or_percent) + pp.Suppress(')')
-        rgb_color.addParseAction(list_parser('rgb', 3))
+        rgb_color.addParseAction(partial(parse_list, 'rgb', 3))
         rgba_color = pp.Suppress('rgba(') + pp.delimitedList(float_or_percent) + pp.Suppress(')')
-        rgba_color.addParseAction(list_parser('rgb', 4))
+        rgba_color.addParseAction(partial(parse_list, 'rgb', 4))
 
         def parse_hex_color(t):
             s = t[0]
             if len(s) not in (3, 4, 6, 8):
                 return pp.ParseFatalException('Invalid number of hex characters.')
-            if len(s) == 3 or len(s) == 4:
+            if len(s) in (3, 4):
                 return ('rgb',) + tuple(int(ch+ch, 16) / 255 for ch in t[0])
-            if len(s) == 6 or len(s) == 8:
+            if len(s) in (6, 8):
                 r = int(s[0:2], 16) / 255
                 g = int(s[2:4], 16) / 255
                 b = int(s[4:6], 16) / 255
